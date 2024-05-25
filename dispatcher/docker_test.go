e0873f4b4f293dc48b22803e4b65f3407f667a53
Yaxiong Zhao <z@sxwl.ai>
2024-05-25 14:02:55 -0700

    Fix test
    Signed-off-by: Yaxiong Zhao <z@sxwl.ai>

--- dispatcher/docker_test.go
+++ dispatcher/docker_test.go
@@ -2,6 +2,7 @@ package dispatcher
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -14,12 +15,13 @@ func TestDockerRun(t *testing.T) {
 	}
 
 	image := "runtime:latest"
-	cmd := []string{}
+	cmd := []string{"python", "runtime.py", "--file=runtime_alpha.py", "--class_name=RuntimeAlpha"}
 	portBindings := map[string]string{
-		"8080": "5000",
+		"8000" /*host port*/ : "5000", /*container port*/
 	}
 
 	assert.Nil(t, docker.Run(image, cmd, portBindings), "Expected no error, got %v", err)
+	time.Sleep(100 * time.Second)
 	assert.Nil(t, docker.Stop(), "Expected no error stopping container, got %v", err)
 	assert.Nil(t, docker.Remove(), "Expected no error removing container, got %v", err)
 }
