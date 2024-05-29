72086b50b9368ac7b899d965379ab4bee17857a1
Yaxiong Zhao <z@sxwl.ai>
2024-05-29 00:50:48 -0700

    Remove extra import time
    Signed-off-by: Yaxiong Zhao <z@sxwl.ai>

--- runtime/runtime_alpha.py
+++ runtime/runtime_alpha.py
@@ -3,12 +3,10 @@ import time
 class RuntimeAlpha:
     def load(self):
         # This function needs to be called every time the container starts.
-        import time
         time.sleep(4)
 
     def generate(self, args):
         # This is just a mock function to simulate processing of the user request.
-        import time
         time.sleep(0.75)
         prompt = args["prompt"]
         return f"Given your question: {prompt}. I think the best answer is to buy ice cream."
